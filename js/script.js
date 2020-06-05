85fe9a0e86b31233926c092a371e8d38b149b56a
FAHIM TAYEBEE <fahimtayebee9@gmail.com>
2020-06-06 01:12:50 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+AOS.init();
+
+$({ Counter: 0 }).animate({
+    Counter: $('.count-num').text()
+}, {
+    duration: 1000,
+    easing: 'swing',
+    step: function() {
+        $('.Single').text((this.Counter));
+    }
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-7742845-9', 'inorganik.github.io');
+    ga('send', 'pageview');
+    
+// Carousel
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
